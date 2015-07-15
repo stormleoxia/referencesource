8f3cae56e1586ff9709ca78e1b3000da5860d132
Martin Baulig <martin.baulig@xamarin.com>
2015-07-15 06:06:01 +0200

    [System]: SslState.AsyncResumeHandshakeRead(): Fix resumption of pending handshake.

--- System/net/System/Net/SecureProtocols/_SslState.cs
+++ System/net/System/Net/SecureProtocols/_SslState.cs
@@ -1664,7 +1664,7 @@ namespace System.Net.Security {
         {
             AsyncProtocolRequest asyncRequest = (AsyncProtocolRequest)state;
             try {
-                if (_PendingReHandshake == 1)
+                if (_PendingReHandshake == 1 && asyncRequest.Buffer == null)
                 {
                     // resume as read a blob
                     StartReceiveBlob(asyncRequest.Buffer, asyncRequest);
